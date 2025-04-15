a05536c0561de4ace9e5fdc19200db7cfd41fe40
Ricsius <hajnal.ricsi@gmail.com>
2025-04-15 21:47:22 +0200

    Register action to counter reducer

--- src/app/store/counter.reducer.ts
+++ src/app/store/counter.reducer.ts
@@ -1,5 +1,10 @@
-import { createReducer } from "@ngrx/store";
+import { createReducer, on } from "@ngrx/store";
+import { decrement, increment } from "./counter.actions";
 
 const initialState = 0;
 
-export const counterReducer = createReducer(initialState);
+export const counterReducer = createReducer(
+    initialState,
+    on(increment, n => n + 1),
+    on(decrement, n => n - 1),
+);
