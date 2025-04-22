6e3e8c2184d616a18f76d8fd85890c85aec95a32
Ricsius <hajnal.ricsi@gmail.com>
2025-04-22 17:33:50 +0200

    Add counter initialization on startup

--- src/app/store/counter.effects.ts
+++ src/app/store/counter.effects.ts
@@ -1,6 +1,6 @@
 import { Actions, createEffect, ofType } from "@ngrx/effects";
-import { decrement, increment } from "./counter.actions";
-import { tap, withLatestFrom } from "rxjs";
+import { decrement, increment, init, set } from "./counter.actions";
+import { switchMap, tap, withLatestFrom, of } from "rxjs";
 import { Injectable } from "@angular/core";
 import { Store } from "@ngrx/store";
 import { selectCount } from "./counter.selectors";
@@ -17,6 +17,18 @@ export class CounterEffects {
     ), 
     { dispatch: false }
 );
+    loadCount = createEffect(() => this.actions$.pipe(
+        ofType(init),
+        switchMap(() => {
+            const storedCounter = localStorage.getItem('count');
+
+            if(storedCounter) {
+                return of(set({value: +storedCounter}))
+            }
+
+            return of(set({value: 0}));
+        })
+    ));
 
     constructor(private actions$: Actions, private store: Store<{counter: number}>) {}
 }
--- src/app/store/counter.reducer.ts
+++ src/app/store/counter.reducer.ts
@@ -1,5 +1,5 @@
 import { createReducer, on } from "@ngrx/store";
-import { decrement, increment } from "./counter.actions";
+import { decrement, increment, set } from "./counter.actions";
 
 const initialState = 0;
 
@@ -7,4 +7,5 @@ export const counterReducer = createReducer(
     initialState,
     on(increment, (n, action) => n + action.value),
     on(decrement, n => n - 1),
+    on(set, (n, action) => action.value)
 );
